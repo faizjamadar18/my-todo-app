1ee11e2abd46c1c11cafb900ad6f97dc50502669
faizjamadar18 <faizjamadar18@gmail.com>
2025-09-17 01:06:50 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className=" bg-white flex sm:justify-around justify-between px-5 h-10 items-center sticky top-0">
+    <div className=" bg-white flex sm:justify-around justify-between px-5 h-10 items-center sticky top-0 z-10">
       <div className="logo font-bold sm:text-2xl text-xl" >TaskMate</div>  
-      <ul className="flex list-none sm:gap-6 gap-1">
+      <ul className="flex list-none sm:gap-6 gap-1 mr-3">
         <li className=' hover:font-bold hover:cursor-pointer w-12'>Home</li>
         <li className=' hover:font-bold hover:cursor-pointer w-12'>YourTask</li>
       </ul>
